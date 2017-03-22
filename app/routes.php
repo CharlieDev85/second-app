39bf17a2d9a14c25b433c6190d868492f13cc89a
CharlieDev <php.developer.charlie@gmail.com>
2017-03-21 21:52:05 -0600

    .gitignore corrected 2 and handling routes

--- app/routes.php
+++ app/routes.php
@@ -20,3 +20,18 @@ Route::get('about', function()
 {
     return 'About content goes here';
 });
+
+Route::get('about/directions', function()
+{
+    return 'Directions goes here';
+});
+
+Route::get('about/{theSubject}', function($theSubject)
+{
+    return $theSubject. ' content goes here';
+});
+
+Route::get('about/classes/{theSubject}', function($theSubject)
+{
+    return "Content about {$theSubject} classes hoes here";
+});
